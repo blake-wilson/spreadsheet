72639b1e801dbd797d54ec067df826798e499b86
Blake Wilson <blake@yellowpapersun.com>
2021-04-13 04:39:20 +0000

    Implement API trait for service

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod models;
 mod parser;
 
+use futures::prelude::*;
+use grpcio::{RpcContext, UnarySink};
+
 #[path = "proto/grpc/api.rs"]
 mod api;
 #[path = "proto/grpc/api_grpc.rs"]
@@ -9,7 +12,30 @@ mod api_grpc;
 #[derive(Clone)]
 struct SpreadsheetService;
 
-// impl api::SpreadsheetApi for SpreadsheetService {}
+impl api_grpc::SpreadsheetApi for SpreadsheetService {
+    fn insert_cells(
+        &mut self,
+        ctx: RpcContext<'_>,
+        req: api::InsertCellsRequest,
+        sink: UnarySink<api::InsertCellsResponse>,
+    ) {
+        for c in req.get_cells() {
+            println!(
+                "inserting cell {:?} at row {:?} and col {:?}",
+                c.get_value(),
+                c.get_col(),
+                c.get_row()
+            );
+        }
+        let mut resp = api::InsertCellsResponse::default();
+        resp.set_numInserted(req.get_cells().len() as i32);
+        let f = sink
+            .success(resp)
+            .map_err(move |e| println!("failed to reply {:?}: {:?}", req, e))
+            .map(|_| ());
+        ctx.spawn(f);
+    }
+}
 
 fn main() {
     let mut tokens = parser::lex("CALL(10 * 3, CALLB(100, 200))");
