911dac89ec877b8639f73d542c0117ec1a556a13
Blake Wilson <blake@yellowpapersun.com>
2021-05-08 21:57:22 +0000

    Simplify some logic

--- src/parser/functions.rs
+++ src/parser/functions.rs
@@ -3,17 +3,42 @@ use super::parser::EvalResult;
 pub fn evaluate_function(name: &str, args: Vec<EvalResult>) -> EvalResult {
     match name.to_uppercase().as_str() {
         "SUM" => sum(args),
+        "AVG" => avg(args),
+        // "MEDIAN" => median(args),
         _ => EvalResult::NonNumeric("".to_owned()),
     }
 }
 
 pub fn sum(args: Vec<EvalResult>) -> EvalResult {
-    let mut total = 0f64;
-    for arg in args {
-        match arg {
-            EvalResult::Numeric(n) => total += n,
-            _ => {} // treat non-numeric values as 0
-        }
-    }
+    let total = args.iter().fold(0f64, |acc, x| match x {
+        &EvalResult::Numeric(n) => acc + n,
+        _ => acc,
+    });
+
     EvalResult::Numeric(total)
 }
+
+pub fn avg(args: Vec<EvalResult>) -> EvalResult {
+    let mut num_numeric = 0;
+    let total = args.iter().fold(0f64, |acc, x| match x {
+        &EvalResult::Numeric(n) => {
+            num_numeric += 1;
+            acc + n
+        }
+        _ => acc,
+    });
+
+    EvalResult::Numeric(total / num_numeric as f64)
+}
+
+// pub fn median(args: Vec<EvalResult>) -> EvalResult {
+//     let mut num_numeric = 0;
+//     let total = args.iter().fold(0f64, |acc, x| match x {
+//         &EvalResult::Numeric(n) => {
+//             num_numeric += 1;
+//             acc + n
+//         }
+//     });
+//
+//     EvalResult::Numeric(total / num_numeric as f64)
+// }
