2d10f35cb8527babb2a06d4ff4d4ccdbb7269921
Blake Wilson <blake@yellowpapersun.com>
2021-05-16 22:28:32 +0000

    Simplify nix shell

--- shell.nix
+++ shell.nix
@@ -1,21 +1,12 @@
-let
-    moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-    nixpkgs = import <nixpkgs> { overlays = [moz_overlay ]; };
-    rustNightlyChannel = (nixpkgs.rustChannelOf { date = "2021-04-07"; channel = "nightly"; }).rust.override {
-        extensions = [
-            "rust-src"
-        ];
-    };
-in
-with nixpkgs;
+{ pkgs ? import <nixpkgs> {}}:
 pkgs.mkShell {
-    name = "moz_overlay_shell";
-    buildInputs = [
-        pkgs.cargo
+    name = "rust-shell";
+    buildInputs = with pkgs; [
+        cargo
         buildPackages.protobuf
-            nodePackages.webpack
-            nodePackages.webpack-cli
-            nodePackages.create-react-app
+            pkgs.nodePackages.webpack
+            pkgs.nodePackages.webpack-cli
+            pkgs.nodePackages.create-react-app
             openssl
             nodejs
             pkg-config
